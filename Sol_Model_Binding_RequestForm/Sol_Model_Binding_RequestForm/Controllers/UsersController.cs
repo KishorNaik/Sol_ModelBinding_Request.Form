e72076790b0b796eb722deab8dbafd73fe23b9d5
Kishor Naik <kishor.naik011.net@gmail.com>
2019-09-02 09:37:11 +0530

    #Code Commit

---
+++ Sol_Model_Binding_RequestForm/Sol_Model_Binding_RequestForm/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Model_Binding_RequestForm.Models;
+
+namespace Sol_Model_Binding_RequestForm.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        [BindProperty]
+        public UserModel Users { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OnSubmit()
+        {
+            // Get Hidden Value from Using Request Form
+            string hiddenData = base.HttpContext.Request.Form["hiddenField"][0]; // "SOME_VALUE"
+
+            return View("Index");
+        }
+    }
+}
